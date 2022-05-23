eb5b6ebba7201f6649a7126496c106dbc7c80d26
Shavaliev Aidar <gabahadlion@gmail.com>
2022-05-23 17:09:24 +0300

    Улюлю

--- Zolotoy_telenok_0.1/Запись.cs
+++ Zolotoy_telenok_0.1/Запись.cs
@@ -19,7 +19,7 @@ namespace Zolotoy_telenok_0._1
         public int ИД_Работнка { get; set; }
         public int ИД_Услуги { get; set; }
         public decimal Сумма { get; set; }
-        public Nullable<System.DateTime> Дата { get; set; }
+        public System.DateTime Дата { get; set; }
     
         public virtual Машина Машина { get; set; }
         public virtual Работник Работник { get; set; }
